041c12cbf2c37493b7303b40385fcb87d1c0389c
Yalcin Tosun <ytosun@netas.com.tr>
2016-01-13 23:11:08 +0200

    Building the framework - connected server and client

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+var express = require('express');
+var app = express();
+
 var qcApi = require('QC.js/qcApi.js').create();
 var userpass = require('./.userpass.json');
+//var testset = [];
 
-qcApi.login({
+/*qcApi.login({
     "server": "http://gbplqc101.genband.com:8080/qcbin",
     "domain": "BBN",
     "project": "CA_CM9500_v1_2",
@@ -18,4 +22,19 @@ function(){
 	function(err) { console.log("something failed: " + err) });
 }, function(err){
     console.log("oh shit, something went awry!")
+});*/
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+    res.send(/*toJSON.stringify(tests)*/"Hello World");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+})
